eb0f2c4f1623619436d7a68aac4beb92e7728ddc
Yas Naoi <github@twinkle.cc>
2020-04-07 03:44:37 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!" by yas
 output "hello_world" {
   value = "Hello, World!"
 }
